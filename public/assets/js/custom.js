0d178adb742ccf8a7fcce420570b6e7fbe0041e0
Munjal Mayank <munjaldevelopment@gmail.com>
2021-02-26 11:51:49 +0530

    public

---
+++ public/assets/js/custom.js
@@ -0,0 +1,112 @@
+$(document).ready(function() {
+	var base_url = $('base').attr('href');
+	
+	var CSRF_TOKEN = $('meta[name="csrf-token"]').attr('content');
+	
+	$('.home-class').bind('click', function() {
+		$('.esskay-home li a').removeClass('active');
+		$('.home-class').addClass('active');
+		
+		$.ajax({
+			url: base_url+'homepage',
+			type: 'post',
+			data: {_token: CSRF_TOKEN},
+			beforeSend: function() {
+				$('.preloader').show();
+			},
+			success: function(output) {
+				$('.preloader').hide();
+				$('.home-content').html(output);
+			}
+		});
+	});
+		
+	$('.doc-class').bind('click', function() {
+		$('.esskay-home li a').removeClass('active');
+		$('.doc-class').addClass('active');
+		
+		$.ajax({
+			url: base_url+'document',
+			type: 'post',
+			data: {_token: CSRF_TOKEN},
+			beforeSend: function() {
+				$('.preloader').show();
+			},
+			success: function(output) {
+				$('.preloader').hide();
+				$('.home-content').html(output);
+			}
+		});
+	});
+
+	$('.news-class').bind('click', function() {
+		$('.esskay-home li a').removeClass('active');
+		$('.news-class').addClass('active');
+		
+		$.ajax({
+			url: base_url+'news',
+			type: 'post',
+			data: {_token: CSRF_TOKEN},
+			beforeSend: function() {
+				$('.preloader').show();
+			},
+			success: function(output) {
+				$('.preloader').hide();
+				$('.home-content').html(output);
+			}
+		});
+	});
+
+	$('.contact-class').bind('click', function() {
+		$('.esskay-home li a').removeClass('active');
+		$('.contact-class').addClass('active');
+		
+		$.ajax({
+			url: base_url+'contact_us',
+			type: 'post',
+			data: {_token: CSRF_TOKEN},
+			beforeSend: function() {
+				$('.preloader').show();
+			},
+			success: function(output) {
+				$('.preloader').hide();
+				$('.home-content').html(output);
+			}
+		});
+	});
+	
+	$('.graph-class').bind('click', function() {
+		$('.esskay-home li a').removeClass('active');
+		$('.graph-class').addClass('active');
+		
+		$.ajax({
+			url: base_url+'company_graph',
+			type: 'post',
+			data: {_token: CSRF_TOKEN},
+			beforeSend: function() {
+				$('.preloader').show();
+			},
+			success: function(output) {
+				$('.preloader').hide();
+				$('.home-content').html(output);
+			}
+		});
+	});
+	
+	var is_message_md = $('#is_message_md').val();
+	var is_document = $('#is_document').val();
+	var is_financial_summary = $('#is_financial_summary').val();
+	
+	if(is_message_md == "1")
+	{
+		$('.home-class').trigger('click');
+	}
+	else if(is_message_md == "0" && is_document == "1")
+	{
+		$('.doc-class').trigger('click');
+	}
+	else if(is_message_md == "0" && is_document == "0" && is_financial_summary == "1")
+	{
+		$('.graph-class').trigger('click');
+	}
+});
---
+++ public/packages/ckeditor/config.js
@@ -0,0 +1,16 @@
+/**
+ * @license Copyright (c) 2003-2020, CKSource - Frederico Knabben. All rights reserved.
+ * For licensing, see https://ckeditor.com/legal/ckeditor-oss-license
+ */
+
+CKEDITOR.editorConfig = function( config ) {
+	// Define changes to default configuration here. For example:
+	// config.language = 'fr';
+	// config.uiColor = '#AADC6E';
+	
+	config.allowedContent = true;
+	
+	config.contentsCss = 'https://fonts.googleapis.com/css?family=Montserrat';
+
+	config.font_names = "Montserrat/Montserrat;Arial/Arial, Helvetica, sans-serif;Comic Sans MS/Comic Sans MS, cursive;Courier New/Courier New, Courier, monospace;Georgia/Georgia, serif;Lucida Sans Unicode/Lucida Sans Unicode, Lucida Grande, sans-serif;Tahoma/Tahoma, Geneva, sans-serif;Times New Roman/Times New Roman, Times, serif;Trebuchet MS/Trebuchet MS, Helvetica, sans-serif;Verdana/Verdana, Geneva, sans-serif";
+};
