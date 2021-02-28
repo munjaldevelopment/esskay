afb61436e6e41ec7bfd0de06e531f66913af54aa
Munjal Mayank <munjaldevelopment@gmail.com>
2021-02-28 23:57:23 +0530

    Update Versions.php

--- vendor/composer/package-versions-deprecated/src/PackageVersions/Versions.php
+++ vendor/composer/package-versions-deprecated/src/PackageVersions/Versions.php
@@ -193,7 +193,7 @@ final class Versions
   'sebastian/version' => '2.0.1@99732be0ddb3361e16ad77b68ba41efc8e979019',
   'theseer/tokenizer' => '1.2.0@75a63c33a8577608444246075ea0af0d052e452a',
   'webmozart/assert' => '1.9.1@bafc69caeb4d49c39fd0779086c03a3738cbb389',
-  'laravel/laravel' => 'dev-main@11a2a85be4ed7929776660da7e4d304dd0bbf669',
+  'laravel/laravel' => 'dev-main@61330983c022410fb222486bd4f249457aff9464',
 );
 
     private function __construct()
