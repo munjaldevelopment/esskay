fce132656c42c61cb58cc382dac48782f7235b0c
Munjal Mayank <munjaldevelopment@gmail.com>
2021-02-28 22:38:25 +0530

    Update ess-kay-homepage.blade.php

--- resources/views/ess-kay-homepage.blade.php
+++ resources/views/ess-kay-homepage.blade.php
@@ -1,7 +1,35 @@
-<div class="container-fluid">
-	<div class="outer-bg">
-		<div class="content-area-custom">
-			{!! $page_content !!}
+<div class="mtd-inner-box">
+	<div class="mtd-inner about-mai-box">
+		<div class="about-video-area">	
+			<div class="row h-100">
+				<div class="col-md-7 col-sm-12">
+					<div class="about-content h-100 justify-content-center d-flex">
+						Lorem Ipsum is simply dummy text of the printing and typesetting industry.
+					</div>									
+				</div>
+				
+				<div class="col-md-5 col-sm-12">
+					<div class="about-video">
+						<div class="made-sk-box">	
+							<div class="more-about-video">			
+								<iframe id="sk_frame1" src="https://www.youtube.com/embed/9xwazD5SyVg" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+								<div class="more-video-cont" id="sk_video_cont">
+									<button type="button" class="speechmax-icon" id="sk_video1">
+										<img src="images/video-bg.png" alt="">
+										<img src="images/play-btn.svg" alt="" class="play-btn-about">
+									</button>
+								</div>				
+							</div>	
+						</div>
+					</div>									
+				</div>
+			</div>	
+		</div>
+		
+		<div class="about-testimonial-area">
+			<div class="content-area-custom">
+				{!! $page_content !!}
+			</div>
 		</div>
 	</div>
 </div>
