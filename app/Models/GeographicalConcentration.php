db54a70e7023b6e9fde829e4fab7a869a0084f15
Munjal Mayank <munjaldevelopment@gmail.com>
2021-03-06 21:00:50 +0530

    Update GeographicalConcentration.php

--- app/Models/GeographicalConcentration.php
+++ app/Models/GeographicalConcentration.php
@@ -19,6 +19,7 @@ class GeographicalConcentration extends Model
     // protected $primaryKey = 'id';
     // public $timestamps = false;
     protected $guarded = ['id'];
+    protected $fillable = ['geographical_diversification', 'docp', 'amount1', 'amount_percentage1', 'amount2', 'amount_percentage2', 'amount3',     'amount_percentage3', 'amount4', 'amount_percentage4', 'amount5', 'amount_percentage5', 'amount6', 'amount_percentage6', 'amount7', 'amount_percentage7', 'amount8', 'amount_percentage8', 'amount9', 'amount_percentage9'];
     // protected $fillable = [];
     // protected $hidden = [];
     // protected $dates = [];
@@ -28,6 +29,13 @@ class GeographicalConcentration extends Model
     | FUNCTIONS
     |--------------------------------------------------------------------------
     */
+    public function exportGeographicalConcentrationButton() {
+        return "<a class='btn btn-success ladda-button tooltipped' data-position='right' data-delay='50' data-tooltip='Export Geographical Concentration'  href='".backpack_url('exportGeographicalConcentration')."'><i class='fa fa-download'></i> Export Geographical Concentration </a> &nbsp;&nbsp;"; 
+    }
+    
+    public function importGeographicalConcentrationButton() {
+        return "<a class='btn btn-success ladda-button tooltipped' data-position='right' data-delay='50' data-tooltip='Import Geographical Concentration'  href='".backpack_url('importGeographicalConcentration')."'><i class='fa fa-cloud'></i> Import Geographical Concentration </a> &nbsp;&nbsp;"; 
+    }
 
     /*
     |--------------------------------------------------------------------------
