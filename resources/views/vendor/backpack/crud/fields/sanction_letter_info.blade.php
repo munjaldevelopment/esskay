4d676115cb157848e6748027585a99bbfb3800c5
Munjal Mayank <munjaldevelopment@gmail.com>
2021-03-19 12:11:55 +0530

    cc

--- resources/views/vendor/backpack/crud/fields/sanction_letter_info.blade.php
+++ resources/views/vendor/backpack/crud/fields/sanction_letter_info.blade.php
@@ -51,9 +51,6 @@
     {{-- FIELD CSS - will be loaded in the after_styles section --}}
     @push('crud_fields_styles')
     <!-- include select2 css-->
-    <link href="{{ asset('vendor/adminlte/bower_components/select2/dist/css/select2.min.css') }}" rel="stylesheet" type="text/css" />
-	<link href="https://select2.github.io/select2-bootstrap-theme/css/select2-bootstrap.css" rel="stylesheet" type="text/css" />
-    <link href="https://cdnjs.cloudflare.com/ajax/libs/select2-bootstrap-theme/0.1.0-beta.10/select2-bootstrap.min.css" rel="stylesheet" type="text/css" />
     @endpush
 
     {{-- FIELD JS - will be loaded in the after_scripts section --}}
