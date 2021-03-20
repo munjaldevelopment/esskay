3025af5b07179c4000e13ea6fc33340fa86bcb91
Munjal Mayank <munjaldevelopment@gmail.com>
2021-03-20 17:17:55 +0530

    Update insight-listing-trustee.blade.php

--- resources/views/insight-listing-trustee.blade.php
+++ resources/views/insight-listing-trustee.blade.php
@@ -27,10 +27,10 @@
 					@endforeach
 				</div>
 				<div class="operation-highlight-year">
-					<div class="pull-left">
+					<div class="ohbh-box-left">
 						{{ $insightFirst->operation_row1_year }}
 					</div>
-					<div class="pull-right">
+					<div class="ohbh-box-right">
 						{{ $insightFirst->operation_row2_year }}
 					</div>
 				</div>
@@ -54,7 +54,7 @@
 					@endforeach
 				</div>
 				<div class="operation-highlight-year">
-					<div class="pull-right">
+					<div class="ohbh-box-right">
 						2021
 					</div>
 				</div>
