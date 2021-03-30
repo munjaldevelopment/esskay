9c687ba0cadb3ef2d403aeaa9bf9016e2f8f82fd
Munjal Mayank <munjaldevelopment@gmail.com>
2021-03-30 14:49:39 +0530

    Update login.blade.php

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -124,7 +124,7 @@
 									<div class="row">  
 										<div class="col-md-12">
 											<div class="user-login-btn">
-											<button type="submit" class="custom-btn btn"><i class="fa fa-sign-in hide" aria-hidden="true"></i> Sign in</button>
+											<button type="submit" class="custom-btn btn"><i class="fa fa-sign-in d-none hide" aria-hidden="true"></i> Sign in</button>
 											</div>
 
 											<p class="text-center">This site is protected by reCAPTCHA</p>
@@ -237,7 +237,7 @@
 									<div class="row">  
 										<div class="col-md-12">
 											<div class="user-login-btn">
-											<button type="button" class="custom-btn btn btn-register"><i class="fa fa-sign-in" aria-hidden="true"></i> Sign in</button>
+											<button type="button" class="custom-btn btn btn-register"><i class="fa fa-sign-in d-none hide" aria-hidden="true"></i> Sign Up</button>
 											</div>
 										</div>  
 									</div>
