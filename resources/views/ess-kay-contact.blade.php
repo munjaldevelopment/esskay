7659d9281fba2d9511eefab0d66f15c40887aa6f
Munjal Mayank <munjaldevelopment@gmail.com>
2021-04-01 11:58:54 +0530

    css

--- resources/views/ess-kay-contact.blade.php
+++ resources/views/ess-kay-contact.blade.php
@@ -94,7 +94,7 @@
 								<div class="row">  
 									<div class="col-md-12">
 										<div class="user-login-btn">
-										<button type="button" class="custom-btn btn btn-contact"><i class="fa fa-sign-in" aria-hidden="true"></i> Submit</button>
+										<button type="button" class="custom-btn btn btn-contact"><i class="fa fa-sign-in d-none hide" aria-hidden="true"></i> Submit</button>
 										</div>
 									</div>  
 								</div>
