1ed1edce3eaf21eb057dddf2e3f848e9d9feeadd
Munjal Mayank <munjaldevelopment@gmail.com>
2021-04-03 17:05:39 +0530

    css

--- resources/views/edit-password.blade.php
+++ resources/views/edit-password.blade.php
@@ -93,7 +93,7 @@
 
 									<div class="row">  
 										<div class="col-md-12">
-											<div class="user-login-btn">
+											<div class="user-login-btn change-passowrd">
 												<button disabled type="button" class="btn-change submit-btn custom-btn1 btn fxt-btn-fill"><i class="fa fa-sign-in d-none hide" aria-hidden="true"></i> Verify </button>
 												<a href="{{ asset('/')}}" class="custom-btn1 btn fxt-btn-fill">Back</a>
 											</div>
