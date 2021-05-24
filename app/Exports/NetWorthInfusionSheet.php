eb98d9766d4ec335f1f0ecdfad93dc2837ffc5c4
Munjal Mayank <munjaldevelopment@gmail.com>
2021-05-24 22:40:22 +0530

    import / export

--- app/Exports/NetWorthInfusionSheet.php
+++ app/Exports/NetWorthInfusionSheet.php
@@ -6,7 +6,7 @@ use Maatwebsite\Excel\Concerns\WithTitle;
 use Maatwebsite\Excel\Concerns\WithHeadings;
 use Maatwebsite\Excel\Concerns\ShouldAutoSize;
 
-use App\Models\NetWorth;
+use App\Models\NetWorthInfusion;
 
 class NetWorthInfusionSheet implements FromArray, WithTitle, WithHeadings, ShouldAutoSize
 {
@@ -20,26 +20,16 @@ class NetWorthInfusionSheet implements FromArray, WithTitle, WithHeadings, Shoul
     {
         return [
             'ID',
-            'Particulars',
-            'Top FY16 Amount',
-			'Top FY17 Amount',
-			'Top FY18 Amount',
-			'Top FY19 Amount',
-			'Top FY20 Amount',
-			'Top FY21 Amount',
-			'Bottom FY16 Amount',
-			'Bottom FY17 Amount',
-			'Bottom FY18 Amount',
-			'Bottom FY19 Amount',
-			'Bottom FY20 Amount',
-			'Bottom FY21 Amount',
+            'Month',
+            'Capital Infusion',
+			'Investors',
             'Status',
         ];
     }
 	
 	public function array(): array
     {
-		$factories = NetWorth::orderBy('id', 'ASC')->get()->toArray();
+		$factories = NetWorthInfusion::orderBy('id', 'ASC')->get()->toArray();
 		//dd($factories);
 		$factoryData = array();
 		if($factories){
@@ -47,20 +37,10 @@ class NetWorthInfusionSheet implements FromArray, WithTitle, WithHeadings, Shoul
 			{
 				$factoryData[$k] = array(
 					'ID' => $factory['id'],
-					'Particulars' => $factory['particulars'],
-					'Top FY16 Amount' => $factory['amount1'],
-					'Top FY17 Amount' => $factory['amount2'],
-					'Top FY18 Amount' => $factory['amount3'],
-					'Top FY19 Amount' => $factory['amount4'],
-					'Top FY20 Amount' => $factory['amount5'],
-					'Top FY21 Amount' => $factory['amount6'],
-					'Bottom FY16 Amount' => $factory['amount7'],
-					'Bottom FY17 Amount' => $factory['amount8'],
-					'Bottom FY18 Amount' => $factory['amount9'],
-					'Bottom FY19 Amount' => $factory['amount10'],
-					'Bottom FY20 Amount' => $factory['amount11'],
-					'Bottom FY21 Amount' => $factory['amount12'],
-		            'Status' => ($factory['net_worth_status'] ? "Yes" : "No"));
+					'Month' => $factory['month'],
+					'Capital Infusion' => $factory['capital_infusion'],
+					'Investors' => $factory['investors'],
+		            'Status' => ($factory['net_worth_infusion_status'] ? "Yes" : "No"));
 			}
 		}
 		
