586e2e6301aa5555d33c51ed8a8b7aef9c249538
Munjal Mayank <munjaldevelopment@gmail.com>
2021-05-25 12:56:19 +0530

    Update welcome.blade.php

--- resources/views/emails/welcome.blade.php
+++ resources/views/emails/welcome.blade.php
@@ -1,20 +1,48 @@
-@extends('beautymail::templates.widgets')
+@extends('beautymail::templates.minty')
 
 @section('content')
 
-	@include('beautymail::templates.widgets.articleStart')
-
-		<h4 class="secondary"><strong>Hello World</strong></h4>
-		<p>This is a test</p>
-
-	@include('beautymail::templates.widgets.articleEnd')
-
-
-	@include('beautymail::templates.widgets.newfeatureStart')
-
-		<h4 class="secondary"><strong>Hello World again</strong></h4>
-		<p>This is another test</p>
-
-	@include('beautymail::templates.widgets.newfeatureEnd')
+	@include('beautymail::templates.minty.contentStart')
+		<tr>
+			<td class="title">
+				Welcome Steve
+			</td>
+		</tr>
+		<tr>
+			<td width="100%" height="10"></td>
+		</tr>
+		<tr>
+			<td class="paragraph">
+				This is a paragraph text
+			</td>
+		</tr>
+		<tr>
+			<td width="100%" height="25"></td>
+		</tr>
+		<tr>
+			<td class="title">
+				This is a heading
+			</td>
+		</tr>
+		<tr>
+			<td width="100%" height="10"></td>
+		</tr>
+		<tr>
+			<td class="paragraph">
+				More paragraph text.
+			</td>
+		</tr>
+		<tr>
+			<td width="100%" height="25"></td>
+		</tr>
+		<tr>
+			<td>
+				@include('beautymail::templates.minty.button', ['text' => 'Sign in', 'link' => '#'])
+			</td>
+		</tr>
+		<tr>
+			<td width="100%" height="25"></td>
+		</tr>
+	@include('beautymail::templates.minty.contentEnd')
 
 @stop
