c9757d1ceed6e9b59ee8a13eb36e871f9b684f11
Munjal Mayank <munjaldevelopment@gmail.com>
2021-07-02 19:39:33 +0530

    Update insight-listing-trustee.blade.php

--- resources/views/insight-listing-trustee.blade.php
+++ resources/views/insight-listing-trustee.blade.php
@@ -959,7 +959,32 @@
 	    	scrollY:        "300px",
         	scrollX:        true,
         	scrollCollapse: true,
-        	fixedColumns: true
+        	fixedColumns: true,
+        	initComplete: function () {
+	            // Apply the search
+	            this.api().columns().every( function (key) {
+	            	var column = this;
+	                if(key <= 1)	                    
+					{
+						var select = $('<select class="form-control1"><option value=""></option></select>')
+		                    .appendTo( $(column.footer()).empty() )
+		                    .on( 'change', function () {
+		                        var val = $.fn.dataTable.util.escapeRegex(
+		                            $(this).val()
+		                        );
+		 
+		                        column
+		                            .search( val ? '^'+val+'$' : '', true, false )
+		                            .draw();
+		                });
+
+					
+		                column.data().unique().sort().each( function ( d, j ) {
+		                    select.append( '<option value="'+d+'">'+d+'</option>' )
+		                } );
+		            }
+	            });
+	        }
 	    });
 	});
 </script>
