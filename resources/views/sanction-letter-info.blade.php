2e83f1dc6564b4a6ec9ef9720bb8818f777a6063
Munjal Mayank <munjaldevelopment@gmail.com>
2021-07-08 14:57:09 +0530

    Update sanction-letter-info.blade.php

--- resources/views/sanction-letter-info.blade.php
+++ resources/views/sanction-letter-info.blade.php
@@ -4,7 +4,8 @@
 		<div class="custom-table-area">
 			<div class="table-responsive">
 				<table class="table">
-					<thead>
+					@if($sanctionLetterData)
+						@foreach($sanctionLetterData as $row)
 						<tr>
 							<th class="text-justify" style="min-width: 140px;">Bank Name</th>
 							<th class="border-bottom">Type of Facility</th>
@@ -30,54 +31,49 @@
 							<th class="border-bottom">Sanction Letter</th>
 							<th style="min-width:120px;" class="border-bottom">Action</th>
 						</tr>
-					</thead>
-					<tbody>
-						@if($sanctionLetterData)
-							@foreach($sanctionLetterData as $row)
-							<tr>
-								<td class="text-justify">{{ $row->bank_name }}</td>
-								<td>{{ $row->type_facility }}</td>
-								<td>{{ $row->facility_amount }}</td>
-								<td>{{ $row->roi }}</td>
-								<td>{{ $row->all_incluside_roi }}</td>
-								<td>{{ $row->processing_fees }}</td>
-								<td>{{ $row->processing_fees_amount }}</td>
-								<td>{{ $row->arranger_fees }}</td>
-								<td>{{ $row->arranger_fees_amount }}</td>
-								<td>{{ $row->other_charges_doc }}</td>
-								<td>{{ $row->total_associated_cost }}</td>
-								<td>{{ $row->all_inclusive_cost }}</td>
-								<td>{!! $row->financial_covenant !!}</td>
-								<td>{!! $row->rationale_availing !!}</td>
-								<td>{{ $row->blended_cost }}</td>
-								<td>{{ $row->stamp_duty_fees }}</td>
-								<td>{{ $row->tenor }}</td>
-								<td>{{ $row->security_cover }}</td>
-								<td>{{ $row->cash_collateral }}</td>
-								<td>{{ $row->personal_guarantee }}</td>
-								<td>{{ $row->intermediary }}</td>
-								<td>{{ $row->sanction_letter }}</td>
-								<td class="accept-sanction-container{{ $row->id }}">
-									@if($trustee_id == 1)
-										@if($row->is_approve1 == 0)<a class="btn btn-info accept-sanction1" data-id="{{ $row->id }}" href="javascript:;"><i class="fa fa-check"></i></a> 
-										&nbsp;&nbsp; 
-										<a class="btn btn-danger reject-sanction1" href="javascript:;"><i class="fa fa-times"></i></a>@endif
-									@elseif($trustee_id == 2)
-										@if($row->is_approve2 == 0)<a class="btn btn-info accept-sanction2" data-id="{{ $row->id }}" href="javascript:;"><i class="fa fa-check"></i></a>
-										&nbsp;&nbsp;
-										<a class="btn btn-danger reject-sanction2" href="javascript:;"><i class="fa fa-times"></i></a>
-										@endif
-									@elseif($trustee_id == 3)
-										@if($row->is_approve3 == 0)<a class="btn btn-info accept-sanction3" data-id="{{ $row->id }}" href="javascript:;"><i class="fa fa-check"></i></a>
-										&nbsp;&nbsp;
-										<a class="btn btn-danger reject-sanction3" href="javascript:;"><i class="fa fa-times"></i></a>
-										@endif
+						<tr>
+							<td class="text-justify">{{ $row->bank_name }}</td>
+							<td>{{ $row->type_facility }}</td>
+							<td>{{ $row->facility_amount }}</td>
+							<td>{{ $row->roi }}</td>
+							<td>{{ $row->all_incluside_roi }}</td>
+							<td>{{ $row->processing_fees }}</td>
+							<td>{{ $row->processing_fees_amount }}</td>
+							<td>{{ $row->arranger_fees }}</td>
+							<td>{{ $row->arranger_fees_amount }}</td>
+							<td>{{ $row->other_charges_doc }}</td>
+							<td>{{ $row->total_associated_cost }}</td>
+							<td>{{ $row->all_inclusive_cost }}</td>
+							<td>{!! $row->financial_covenant !!}</td>
+							<td>{!! $row->rationale_availing !!}</td>
+							<td>{{ $row->blended_cost }}</td>
+							<td>{{ $row->stamp_duty_fees }}</td>
+							<td>{{ $row->tenor }}</td>
+							<td>{{ $row->security_cover }}</td>
+							<td>{{ $row->cash_collateral }}</td>
+							<td>{{ $row->personal_guarantee }}</td>
+							<td>{{ $row->intermediary }}</td>
+							<td>{{ $row->sanction_letter }}</td>
+							<td class="accept-sanction-container{{ $row->id }}">
+								@if($trustee_id == 1)
+									@if($row->is_approve1 == 0)<a class="btn btn-info accept-sanction1" data-id="{{ $row->id }}" href="javascript:;"><i class="fa fa-check"></i></a> 
+									&nbsp;&nbsp; 
+									<a class="btn btn-danger reject-sanction1" href="javascript:;"><i class="fa fa-times"></i></a>@endif
+								@elseif($trustee_id == 2)
+									@if($row->is_approve2 == 0)<a class="btn btn-info accept-sanction2" data-id="{{ $row->id }}" href="javascript:;"><i class="fa fa-check"></i></a>
+									&nbsp;&nbsp;
+									<a class="btn btn-danger reject-sanction2" href="javascript:;"><i class="fa fa-times"></i></a>
 									@endif
-								</td>
-							</tr>
-							@endforeach
-						@endif
-					</tbody>
+								@elseif($trustee_id == 3)
+									@if($row->is_approve3 == 0)<a class="btn btn-info accept-sanction3" data-id="{{ $row->id }}" href="javascript:;"><i class="fa fa-check"></i></a>
+									&nbsp;&nbsp;
+									<a class="btn btn-danger reject-sanction3" href="javascript:;"><i class="fa fa-times"></i></a>
+									@endif
+								@endif
+							</td>
+						</tr>
+					@endforeach
+					@endif
 				</table>	
 			</div>	
 		</div>
