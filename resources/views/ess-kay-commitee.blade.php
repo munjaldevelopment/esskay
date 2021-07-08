958aae88e31c6b98c7820ace44276952353a9ee8
Munjal Mayank <munjaldevelopment@gmail.com>
2021-07-08 18:50:48 +0530

    Update ess-kay-commitee.blade.php

--- resources/views/ess-kay-commitee.blade.php
+++ resources/views/ess-kay-commitee.blade.php
@@ -60,7 +60,7 @@
 
 				<div class="white-box outstanding-box">
 					<div class="outstanding-table">
-						<h3>Composition of Board of Directors:</h3>
+						<h3>Composition of Asset Liability Committee</h3>
 						<div class="custom-table-area">
 							<div class="table-responsive">
 								<table class="table">
@@ -72,7 +72,7 @@
 										</tr>
 									</thead>
 									<tbody>
-										@foreach($boardData as $row)
+										@foreach($assetData as $row)
 										<tr>
 											<td class="text-justify">{{ $row->director_name }}</td>
 											<td>{{ $row->status }}</td>
@@ -88,7 +88,7 @@
 
 				<div class="white-box outstanding-box">
 					<div class="outstanding-table">
-						<h3>Composition of Board of Directors:</h3>
+						<h3>Composition of Risk Management Committee</h3>
 						<div class="custom-table-area">
 							<div class="table-responsive">
 								<table class="table">
@@ -100,7 +100,7 @@
 										</tr>
 									</thead>
 									<tbody>
-										@foreach($boardData as $row)
+										@foreach($riskData as $row)
 										<tr>
 											<td class="text-justify">{{ $row->director_name }}</td>
 											<td>{{ $row->status }}</td>
@@ -116,7 +116,7 @@
 
 				<div class="white-box outstanding-box">
 					<div class="outstanding-table">
-						<h3>Composition of Board of Directors:</h3>
+						<h3>Composition of Corporate Social Responsibility Committee</h3>
 						<div class="custom-table-area">
 							<div class="table-responsive">
 								<table class="table">
@@ -128,7 +128,7 @@
 										</tr>
 									</thead>
 									<tbody>
-										@foreach($boardData as $row)
+										@foreach($corpData as $row)
 										<tr>
 											<td class="text-justify">{{ $row->director_name }}</td>
 											<td>{{ $row->status }}</td>
@@ -144,7 +144,7 @@
 
 				<div class="white-box outstanding-box">
 					<div class="outstanding-table">
-						<h3>Composition of Board of Directors:</h3>
+						<h3>Composition of Nomination & Remuneration Committee</h3>
 						<div class="custom-table-area">
 							<div class="table-responsive">
 								<table class="table">
@@ -156,7 +156,7 @@
 										</tr>
 									</thead>
 									<tbody>
-										@foreach($boardData as $row)
+										@foreach($nominationData as $row)
 										<tr>
 											<td class="text-justify">{{ $row->director_name }}</td>
 											<td>{{ $row->status }}</td>
@@ -172,7 +172,7 @@
 
 				<div class="white-box outstanding-box">
 					<div class="outstanding-table">
-						<h3>Composition of Board of Directors:</h3>
+						<h3>Composition of IT Strategy Committee</h3>
 						<div class="custom-table-area">
 							<div class="table-responsive">
 								<table class="table">
@@ -184,7 +184,35 @@
 										</tr>
 									</thead>
 									<tbody>
-										@foreach($boardData as $row)
+										@foreach($itstrategyData as $row)
+										<tr>
+											<td class="text-justify">{{ $row->director_name }}</td>
+											<td>{{ $row->status }}</td>
+											<td>{{ $row->nature_directorship }}</td>
+										</tr>
+										@endforeach
+									</tbody>
+								</table>	
+							</div>	
+						</div>
+					</div>
+				</div>
+
+				<div class="white-box outstanding-box">
+					<div class="outstanding-table">
+						<h3>Composition of Executive Committee</h3>
+						<div class="custom-table-area">
+							<div class="table-responsive">
+								<table class="table">
+									<thead>
+										<tr>
+											<th class="text-justify" style="min-width: 140px;">Name of the Director</th>
+											<th class="border-bottom">Status</th>
+											<th class="border-bottom">Nature of Directorship</th>
+										</tr>
+									</thead>
+									<tbody>
+										@foreach($executiveData as $row)
 										<tr>
 											<td class="text-justify">{{ $row->director_name }}</td>
 											<td>{{ $row->status }}</td>
