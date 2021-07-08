b311fab9a9920a21aa3f522110d36e27bb9de005
Munjal Mayank <munjaldevelopment@gmail.com>
2021-07-08 17:28:41 +0530

    Update sanction-letter-info.blade.php

--- resources/views/sanction-letter-info.blade.php
+++ resources/views/sanction-letter-info.blade.php
@@ -1,126 +1,38 @@
 <div class="white-box outstanding-box">
 	<div class="outstanding-table">
 		<h3>Sanction Letters</h3>
-		@if($sanctionLetterData)
-			@foreach($sanctionLetterData as $row)
-			<div class="custom-table-area">
-				<div class="table-responsive">
-					<table class="table">
+		<div class="custom-table-area">
+			<div class="table-responsive">
+				<table class="table">
+					<thead>
 						<tr>
-							<th style="width:50%;" class="border-bottom">Bank Name</th>
-							<td style="width:50%;">{{ $row->bank_name }}</td>
-						</tr>
-						<tr>
+							<th class="text-justify" style="min-width: 140px;">Bank Name</th>
 							<th class="border-bottom">Type of Facility</th>
-							<td>{{ $row->type_facility }}</td>
-						</tr>
-						<tr>
 							<th class="border-bottom">Facility Amount</th>
-							<td>{{ $row->facility_amount }}</td>
-						</tr>
-						<tr>
 							<th class="border-bottom">ROI</th>
-							<td>{{ $row->roi }}</td>
-						</tr>
-						<tr>
 							<th class="border-bottom">All-inclusive ROI</th>
-							<td>{{ $row->all_incluside_roi }}</td>
-						</tr>
-						<tr>
-							<th class="border-bottom">Processing Fees %</th>
-							<td>{{ $row->processing_fees }}</td>
-						</tr>
-						<tr>
-							<th class="border-bottom">Processing Fees Amount</th>
-							<td>{{ $row->processing_fees_amount }}</td>
-						</tr>
-						<tr>
-							<th class="border-bottom">Arranger Fees %</th>
-							<td>{{ $row->arranger_fees }}</td>
-						</tr>
-						<tr>
-							<th class="border-bottom">Arranger Fees Amount</th>
-							<td>{{ $row->arranger_fees_amount }}</td>
-						</tr>
-						<tr>
-							<th class="border-bottom">Other Charges Doc</th>
-							<td>{{ $row->other_charges_doc }}</td>
-						</tr>
-						<tr>
-							<th class="border-bottom">Total Associated Cost</th>
-							<td>{{ $row->total_associated_cost }}</td>
-						</tr>
-						<tr>
-							<th class="border-bottom">All Inclusive Cost</th>
-							<td>{{ $row->all_inclusive_cost }}</td>
-						</tr>
-						<tr>
-							<th class="border-bottom">Financial Covenant</th>
-							<td>{!! $row->financial_covenant !!}</td>
-						</tr>
-						<tr>
-							<th class="border-bottom">Rationale for Availing facility</th>
-							<td>{!! $row->rationale_availing !!}</td>
-						</tr>
-						<tr>
-							<th class="border-bottom">Blended Cost</th>
-							<td>{{ $row->blended_cost }}</td>
-						</tr>
-						<tr>
-							<th class="border-bottom">Stamp Duty Fees</th>
-							<td>{{ $row->stamp_duty_fees }}</td>
-						</tr>
-						<tr>
-							<th class="border-bottom">Tenor</th>
-							<td>{{ $row->tenor }}</td>
-						</tr>
-						<tr>
-							<th class="border-bottom">Security Cover</th>
-							<td>{{ $row->security_cover }}</td>
-						</tr>
-						<tr>
-							<th class="border-bottom">Cash Collateral</th>
-							<td>{{ $row->cash_collateral }}</td>
-						</tr>
-						<tr>
-							<th class="border-bottom">Personal Guarantee</th>
-							<td>{{ $row->personal_guarantee }}</td>
-						</tr>
-						<tr>
-							<th class="border-bottom">Intermediary</th>
-							<td>{{ $row->intermediary }}</td>
-						</tr>
-						<tr>
-							<th class="border-bottom">Sanction Letter</th>
-							<td><img src="{{ $row->sanction_letter }}" class="img-fluid" /></td>
-						</tr>
-						<tr>
 							<th style="min-width:120px;" class="border-bottom">Action</th>
-							<td class="accept-sanction-container{{ $row->id }}">
-								@if($trustee_id == 1)
-									@if($row->is_approve1 == 0)<a class="btn btn-info accept-sanction1" data-id="{{ $row->id }}" href="javascript:;"><i class="fa fa-check"></i></a> 
-									&nbsp;&nbsp; 
-									<a class="btn btn-danger reject-sanction1" data-id="{{ $row->id }}" href="javascript:;"><i class="fa fa-times"></i></a>@endif
-								@elseif($trustee_id == 2)
-									@if($row->is_approve2 == 0)<a class="btn btn-info accept-sanction2" data-id="{{ $row->id }}" href="javascript:;"><i class="fa fa-check"></i></a>
-									&nbsp;&nbsp;
-									<a class="btn btn-danger reject-sanction2" data-id="{{ $row->id }}" href="javascript:;"><i class="fa fa-times"></i></a>
-									@endif
-								@elseif($trustee_id == 3)
-									@if($row->is_approve3 == 0)<a class="btn btn-info accept-sanction3" data-id="{{ $row->id }}" href="javascript:;"><i class="fa fa-check"></i></a>
-									&nbsp;&nbsp;
-									<a class="btn btn-danger reject-sanction3" data-id="{{ $row->id }}" href="javascript:;"><i class="fa fa-times"></i></a>
-									@endif
-								@endif
-							</td>
 						</tr>
-					</table>
-				</div>	
-			</div>
-
-			<br /><br />
-			@endforeach
-		@endif
+					</thead>
+					<tbody>
+						@if($sanctionLetterData)
+							@foreach($sanctionLetterData as $row)
+							<tr>
+								<td class="text-justify">{{ $row->bank_name }}</td>
+								<td>{{ $row->type_facility }}</td>
+								<td>{{ $row->facility_amount }}</td>
+								<td>{{ $row->roi }}</td>
+								<td>{{ $row->all_incluside_roi }}</td>
+								<td class="accept-sanction-container{{ $row->id }}">
+									<a class="btn btn-info accept-sanction1" href="javascript:;"><i class="fa fa-eye"></i></a>
+								</td>
+							</tr>
+							@endforeach
+						@endif
+					</tbody>
+				</table>	
+			</div>	
+		</div>
 	</div>
 </div>
 
@@ -143,245 +55,6 @@ $(document).ready(function() {
 	
 	var CSRF_TOKEN = $('meta[name="csrf-token"]').attr('content');
 
-	$('.accept-sanction1').bind('click', function() {
-		var sanction_id = $(this).attr('data-id');
-
-		Swal.fire({
-			title: 'Are you sure?',
-			type: 'error',
-	        showCancelButton: true,
-	        confirmButtonColor: '#36c6d3',
-	        cancelButtonColor: '#d33',
-	        confirmButtonText: 'OK',
-	        cancelButtonText: 'Cancel'
-	    }).then((res) => {
-	    	if(res.isConfirmed){
-				$.ajax({
-					url: base_url+'approveSanctionLetter1',
-					type: 'post',
-					data: {_token: CSRF_TOKEN, sanction_id: sanction_id},
-					beforeSend: function() {
-						var content = $('.preloader_doc').html();
-					},
-					success: function(output) {
-						$('.accept-sanction-container'+sanction_id).addClass('d-none');
-
-						Swal.fire({
-							title: 'Approved',
-							title: 'Your file has been approved.',
-							type: 'success'
-					    });
-					}
-				});
-			} else {
-	        	Swal.fire({
-					title: 'Cancelled',
-					title: 'Your file has been cancelled.',
-					type: 'warning'
-			    });
-			}
-		});
-	});
-
-	$('.accept-sanction2').bind('click', function() {
-		var sanction_id = $(this).attr('data-id');
-
-		Swal.fire({
-			title: 'Are you sure?',
-			type: 'error',
-	        showCancelButton: true,
-	        confirmButtonColor: '#36c6d3',
-	        cancelButtonColor: '#d33',
-	        confirmButtonText: 'OK',
-	        cancelButtonText: 'Cancel'
-	    }).then((res) => {
-	    	if(res.isConfirmed){
-	    		$.ajax({
-					url: base_url+'approveSanctionLetter2',
-					type: 'post',
-					data: {_token: CSRF_TOKEN, sanction_id: sanction_id},
-					beforeSend: function() {
-						var content = $('.preloader_doc').html();
-					},
-					success: function(output) {
-						$('.accept-sanction-container'+sanction_id).addClass('d-none');
-
-						Swal.fire({
-							title: 'Approved',
-							title: 'Your file has been approved.',
-							type: 'success'
-					    });
-					}
-				});
-			} else {
-	        	Swal.fire({
-					title: 'Cancelled',
-					title: 'Your file has been cancelled.',
-					type: 'warning'
-			    });
-			}
-		});
-	});
-
-	$('.accept-sanction3').bind('click', function() {
-		var sanction_id = $(this).attr('data-id');
-
-		Swal.fire({
-			title: 'Are you sure?',
-			type: 'error',
-	        showCancelButton: true,
-	        confirmButtonColor: '#36c6d3',
-	        cancelButtonColor: '#d33',
-	        confirmButtonText: 'OK',
-	        cancelButtonText: 'Cancel'
-	    }).then((res) => {
-	    	if(res.isConfirmed){
-				$.ajax({
-					url: base_url+'approveSanctionLetter3',
-					type: 'post',
-					data: {_token: CSRF_TOKEN, sanction_id: sanction_id},
-					beforeSend: function() {
-						var content = $('.preloader_doc').html();
-					},
-					success: function(output) {
-						$('.accept-sanction-container'+sanction_id).addClass('d-none');
-
-						Swal.fire({
-							title: 'Approved',
-							title: 'Your file has been approved.',
-							type: 'success'
-					    });
-					}
-				});
-			} else {
-	        	Swal.fire({
-					title: 'Cancelled',
-					title: 'Your file has been cancelled.',
-					type: 'warning'
-			    });
-			}
-		});
-	});
-
-	// Reject
-	$('.reject-sanction1').bind('click', function() {
-		var sanction_id = $(this).attr('data-id');
-
-		Swal.fire({
-			title: 'Are you sure?',
-			type: 'error',
-	        showCancelButton: true,
-	        confirmButtonColor: '#36c6d3',
-	        cancelButtonColor: '#d33',
-	        confirmButtonText: 'OK',
-	        cancelButtonText: 'Cancel'
-	    }).then((res) => {
-	    	if(res.isConfirmed){
-				$.ajax({
-					url: base_url+'rejectSanctionLetter1',
-					type: 'post',
-					data: {_token: CSRF_TOKEN, sanction_id: sanction_id},
-					beforeSend: function() {
-						var content = $('.preloader_doc').html();
-					},
-					success: function(output) {
-						$('.accept-sanction-container'+sanction_id).addClass('d-none');
-
-						Swal.fire({
-							title: 'Rejected',
-							title: 'Your file has been rejected.',
-							type: 'success'
-					    });
-					}
-				});
-			} else {
-	        	Swal.fire({
-					title: 'Cancelled',
-					title: 'Your file has been cancelled.',
-					type: 'warning'
-			    });
-			}
-		});
-	});
-
-	$('.reject-sanction2').bind('click', function() {
-		var sanction_id = $(this).attr('data-id');
-
-		Swal.fire({
-			title: 'Are you sure?',
-			type: 'error',
-	        showCancelButton: true,
-	        confirmButtonColor: '#36c6d3',
-	        cancelButtonColor: '#d33',
-	        confirmButtonText: 'OK',
-	        cancelButtonText: 'Cancel'
-	    }).then((res) => {
-	    	if(res.isConfirmed){
-	    		$.ajax({
-					url: base_url+'rejectSanctionLetter2',
-					type: 'post',
-					data: {_token: CSRF_TOKEN, sanction_id: sanction_id},
-					beforeSend: function() {
-						var content = $('.preloader_doc').html();
-					},
-					success: function(output) {
-						$('.accept-sanction-container'+sanction_id).addClass('d-none');
-
-						Swal.fire({
-							title: 'Rejected',
-							title: 'Your file has been reject.',
-							type: 'success'
-					    });
-					}
-				});
-			} else {
-	        	Swal.fire({
-					title: 'Cancelled',
-					title: 'Your file has been cancelled.',
-					type: 'warning'
-			    });
-			}
-		});
-	});
-
-	$('.reject-sanction3').bind('click', function() {
-		var sanction_id = $(this).attr('data-id');
-
-		Swal.fire({
-			title: 'Are you sure?',
-			type: 'error',
-	        showCancelButton: true,
-	        confirmButtonColor: '#36c6d3',
-	        cancelButtonColor: '#d33',
-	        confirmButtonText: 'OK',
-	        cancelButtonText: 'Cancel'
-	    }).then((res) => {
-	    	if(res.isConfirmed){
-				$.ajax({
-					url: base_url+'rejectSanctionLetter3',
-					type: 'post',
-					data: {_token: CSRF_TOKEN, sanction_id: sanction_id},
-					beforeSend: function() {
-						var content = $('.preloader_doc').html();
-					},
-					success: function(output) {
-						$('.accept-sanction-container'+sanction_id).addClass('d-none');
-
-						Swal.fire({
-							title: 'Rejected',
-							title: 'Your file has been rejected.',
-							type: 'success'
-					    });
-					}
-				});
-			} else {
-	        	Swal.fire({
-					title: 'Cancelled',
-					title: 'Your file has been cancelled.',
-					type: 'warning'
-			    });
-			}
-		});
-	});
+	
 });
 </script>
