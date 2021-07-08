6336cb21fa7cc79eab79c4d370055be3bec139c7
Munjal Mayank <munjaldevelopment@gmail.com>
2021-07-08 18:44:23 +0530

    Update ess-kay-commitee.blade.php

--- resources/views/ess-kay-commitee.blade.php
+++ resources/views/ess-kay-commitee.blade.php
@@ -1,195 +1,203 @@
-<div class="white-box outstanding-box">
-	<div class="outstanding-table">
-		<h3>Composition of Board of Directors:</h3>
-		<div class="custom-table-area">
-			<div class="table-responsive">
-				<table class="table">
-					<thead>
-						<tr>
-							<th class="text-justify" style="min-width: 140px;">Name of the Director</th>
-							<th class="border-bottom">Status</th>
-							<th class="border-bottom">Nature of Directorship</th>
-						</tr>
-					</thead>
-					<tbody>
-						@foreach($boardData as $row)
-						<tr>
-							<td class="text-justify">{{ $row->director_name }}</td>
-							<td>{{ $row->status }}</td>
-							<td>{{ $row->nature_directorship }}</td>
-						</tr>
-						@endforeach
-					</tbody>
-				</table>	
-			</div>	
-		</div>
-	</div>
-</div>
+<div class="mtd-inner-box">
+	<div class="mtd-inner about-mai-box">
+		<div class="about-testimonial-area">
+			<div class="content-area-custom">
+				<div class="white-box outstanding-box">
+					<div class="outstanding-table">
+						<h3>Composition of Board of Directors:</h3>
+						<div class="custom-table-area">
+							<div class="table-responsive">
+								<table class="table">
+									<thead>
+										<tr>
+											<th class="text-justify" style="min-width: 140px;">Name of the Director</th>
+											<th class="border-bottom">Status</th>
+											<th class="border-bottom">Nature of Directorship</th>
+										</tr>
+									</thead>
+									<tbody>
+										@foreach($boardData as $row)
+										<tr>
+											<td class="text-justify">{{ $row->director_name }}</td>
+											<td>{{ $row->status }}</td>
+											<td>{{ $row->nature_directorship }}</td>
+										</tr>
+										@endforeach
+									</tbody>
+								</table>	
+							</div>	
+						</div>
+					</div>
+				</div>
 
-<div class="white-box outstanding-box">
-	<div class="outstanding-table">
-		<h3>Composition of Board of Directors:</h3>
-		<div class="custom-table-area">
-			<div class="table-responsive">
-				<table class="table">
-					<thead>
-						<tr>
-							<th class="text-justify" style="min-width: 140px;">Name of the Director</th>
-							<th class="border-bottom">Status</th>
-							<th class="border-bottom">Nature of Directorship</th>
-						</tr>
-					</thead>
-					<tbody>
-						@foreach($boardData as $row)
-						<tr>
-							<td class="text-justify">{{ $row->director_name }}</td>
-							<td>{{ $row->status }}</td>
-							<td>{{ $row->nature_directorship }}</td>
-						</tr>
-						@endforeach
-					</tbody>
-				</table>	
-			</div>	
-		</div>
-	</div>
-</div>
+				<div class="white-box outstanding-box">
+					<div class="outstanding-table">
+						<h3>Composition of Board of Directors:</h3>
+						<div class="custom-table-area">
+							<div class="table-responsive">
+								<table class="table">
+									<thead>
+										<tr>
+											<th class="text-justify" style="min-width: 140px;">Name of the Director</th>
+											<th class="border-bottom">Status</th>
+											<th class="border-bottom">Nature of Directorship</th>
+										</tr>
+									</thead>
+									<tbody>
+										@foreach($boardData as $row)
+										<tr>
+											<td class="text-justify">{{ $row->director_name }}</td>
+											<td>{{ $row->status }}</td>
+											<td>{{ $row->nature_directorship }}</td>
+										</tr>
+										@endforeach
+									</tbody>
+								</table>	
+							</div>	
+						</div>
+					</div>
+				</div>
 
-<div class="white-box outstanding-box">
-	<div class="outstanding-table">
-		<h3>Composition of Board of Directors:</h3>
-		<div class="custom-table-area">
-			<div class="table-responsive">
-				<table class="table">
-					<thead>
-						<tr>
-							<th class="text-justify" style="min-width: 140px;">Name of the Director</th>
-							<th class="border-bottom">Status</th>
-							<th class="border-bottom">Nature of Directorship</th>
-						</tr>
-					</thead>
-					<tbody>
-						@foreach($boardData as $row)
-						<tr>
-							<td class="text-justify">{{ $row->director_name }}</td>
-							<td>{{ $row->status }}</td>
-							<td>{{ $row->nature_directorship }}</td>
-						</tr>
-						@endforeach
-					</tbody>
-				</table>	
-			</div>	
-		</div>
-	</div>
-</div>
+				<div class="white-box outstanding-box">
+					<div class="outstanding-table">
+						<h3>Composition of Board of Directors:</h3>
+						<div class="custom-table-area">
+							<div class="table-responsive">
+								<table class="table">
+									<thead>
+										<tr>
+											<th class="text-justify" style="min-width: 140px;">Name of the Director</th>
+											<th class="border-bottom">Status</th>
+											<th class="border-bottom">Nature of Directorship</th>
+										</tr>
+									</thead>
+									<tbody>
+										@foreach($boardData as $row)
+										<tr>
+											<td class="text-justify">{{ $row->director_name }}</td>
+											<td>{{ $row->status }}</td>
+											<td>{{ $row->nature_directorship }}</td>
+										</tr>
+										@endforeach
+									</tbody>
+								</table>	
+							</div>	
+						</div>
+					</div>
+				</div>
 
-<div class="white-box outstanding-box">
-	<div class="outstanding-table">
-		<h3>Composition of Board of Directors:</h3>
-		<div class="custom-table-area">
-			<div class="table-responsive">
-				<table class="table">
-					<thead>
-						<tr>
-							<th class="text-justify" style="min-width: 140px;">Name of the Director</th>
-							<th class="border-bottom">Status</th>
-							<th class="border-bottom">Nature of Directorship</th>
-						</tr>
-					</thead>
-					<tbody>
-						@foreach($boardData as $row)
-						<tr>
-							<td class="text-justify">{{ $row->director_name }}</td>
-							<td>{{ $row->status }}</td>
-							<td>{{ $row->nature_directorship }}</td>
-						</tr>
-						@endforeach
-					</tbody>
-				</table>	
-			</div>	
-		</div>
-	</div>
-</div>
+				<div class="white-box outstanding-box">
+					<div class="outstanding-table">
+						<h3>Composition of Board of Directors:</h3>
+						<div class="custom-table-area">
+							<div class="table-responsive">
+								<table class="table">
+									<thead>
+										<tr>
+											<th class="text-justify" style="min-width: 140px;">Name of the Director</th>
+											<th class="border-bottom">Status</th>
+											<th class="border-bottom">Nature of Directorship</th>
+										</tr>
+									</thead>
+									<tbody>
+										@foreach($boardData as $row)
+										<tr>
+											<td class="text-justify">{{ $row->director_name }}</td>
+											<td>{{ $row->status }}</td>
+											<td>{{ $row->nature_directorship }}</td>
+										</tr>
+										@endforeach
+									</tbody>
+								</table>	
+							</div>	
+						</div>
+					</div>
+				</div>
 
-<div class="white-box outstanding-box">
-	<div class="outstanding-table">
-		<h3>Composition of Board of Directors:</h3>
-		<div class="custom-table-area">
-			<div class="table-responsive">
-				<table class="table">
-					<thead>
-						<tr>
-							<th class="text-justify" style="min-width: 140px;">Name of the Director</th>
-							<th class="border-bottom">Status</th>
-							<th class="border-bottom">Nature of Directorship</th>
-						</tr>
-					</thead>
-					<tbody>
-						@foreach($boardData as $row)
-						<tr>
-							<td class="text-justify">{{ $row->director_name }}</td>
-							<td>{{ $row->status }}</td>
-							<td>{{ $row->nature_directorship }}</td>
-						</tr>
-						@endforeach
-					</tbody>
-				</table>	
-			</div>	
-		</div>
-	</div>
-</div>
+				<div class="white-box outstanding-box">
+					<div class="outstanding-table">
+						<h3>Composition of Board of Directors:</h3>
+						<div class="custom-table-area">
+							<div class="table-responsive">
+								<table class="table">
+									<thead>
+										<tr>
+											<th class="text-justify" style="min-width: 140px;">Name of the Director</th>
+											<th class="border-bottom">Status</th>
+											<th class="border-bottom">Nature of Directorship</th>
+										</tr>
+									</thead>
+									<tbody>
+										@foreach($boardData as $row)
+										<tr>
+											<td class="text-justify">{{ $row->director_name }}</td>
+											<td>{{ $row->status }}</td>
+											<td>{{ $row->nature_directorship }}</td>
+										</tr>
+										@endforeach
+									</tbody>
+								</table>	
+							</div>	
+						</div>
+					</div>
+				</div>
 
-<div class="white-box outstanding-box">
-	<div class="outstanding-table">
-		<h3>Composition of Board of Directors:</h3>
-		<div class="custom-table-area">
-			<div class="table-responsive">
-				<table class="table">
-					<thead>
-						<tr>
-							<th class="text-justify" style="min-width: 140px;">Name of the Director</th>
-							<th class="border-bottom">Status</th>
-							<th class="border-bottom">Nature of Directorship</th>
-						</tr>
-					</thead>
-					<tbody>
-						@foreach($boardData as $row)
-						<tr>
-							<td class="text-justify">{{ $row->director_name }}</td>
-							<td>{{ $row->status }}</td>
-							<td>{{ $row->nature_directorship }}</td>
-						</tr>
-						@endforeach
-					</tbody>
-				</table>	
-			</div>	
-		</div>
-	</div>
-</div>
+				<div class="white-box outstanding-box">
+					<div class="outstanding-table">
+						<h3>Composition of Board of Directors:</h3>
+						<div class="custom-table-area">
+							<div class="table-responsive">
+								<table class="table">
+									<thead>
+										<tr>
+											<th class="text-justify" style="min-width: 140px;">Name of the Director</th>
+											<th class="border-bottom">Status</th>
+											<th class="border-bottom">Nature of Directorship</th>
+										</tr>
+									</thead>
+									<tbody>
+										@foreach($boardData as $row)
+										<tr>
+											<td class="text-justify">{{ $row->director_name }}</td>
+											<td>{{ $row->status }}</td>
+											<td>{{ $row->nature_directorship }}</td>
+										</tr>
+										@endforeach
+									</tbody>
+								</table>	
+							</div>	
+						</div>
+					</div>
+				</div>
 
-<div class="white-box outstanding-box">
-	<div class="outstanding-table">
-		<h3>Composition of Board of Directors:</h3>
-		<div class="custom-table-area">
-			<div class="table-responsive">
-				<table class="table">
-					<thead>
-						<tr>
-							<th class="text-justify" style="min-width: 140px;">Name of the Director</th>
-							<th class="border-bottom">Status</th>
-							<th class="border-bottom">Nature of Directorship</th>
-						</tr>
-					</thead>
-					<tbody>
-						@foreach($boardData as $row)
-						<tr>
-							<td class="text-justify">{{ $row->director_name }}</td>
-							<td>{{ $row->status }}</td>
-							<td>{{ $row->nature_directorship }}</td>
-						</tr>
-						@endforeach
-					</tbody>
-				</table>	
-			</div>	
+				<div class="white-box outstanding-box">
+					<div class="outstanding-table">
+						<h3>Composition of Board of Directors:</h3>
+						<div class="custom-table-area">
+							<div class="table-responsive">
+								<table class="table">
+									<thead>
+										<tr>
+											<th class="text-justify" style="min-width: 140px;">Name of the Director</th>
+											<th class="border-bottom">Status</th>
+											<th class="border-bottom">Nature of Directorship</th>
+										</tr>
+									</thead>
+									<tbody>
+										@foreach($boardData as $row)
+										<tr>
+											<td class="text-justify">{{ $row->director_name }}</td>
+											<td>{{ $row->status }}</td>
+											<td>{{ $row->nature_directorship }}</td>
+										</tr>
+										@endforeach
+									</tbody>
+								</table>	
+							</div>	
+						</div>
+					</div>
+				</div>
+			</div>
 		</div>
 	</div>
 </div>
