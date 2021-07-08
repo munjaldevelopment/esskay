9ad3e79fa496a15949682446c317e7d492191c8f
Munjal Mayank <munjaldevelopment@gmail.com>
2021-07-08 12:37:49 +0530

    Update SanctionLetterCrudController.php

--- app/Http/Controllers/Admin/SanctionLetterCrudController.php
+++ app/Http/Controllers/Admin/SanctionLetterCrudController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\Admin;
 use App\Http\Requests\SanctionLetterRequest;
 use Backpack\CRUD\app\Http\Controllers\CrudController;
 use Backpack\CRUD\app\Library\CrudPanel\CrudPanelFacade as CRUD;
+use Mail;
 
 /**
  * Class SanctionLetterCrudController
