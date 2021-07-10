9268d41ca861fab26109836372fe3035de5977c6
Munjal Mayank <munjaldevelopment@gmail.com>
2021-07-10 19:35:53 +0530

    Update HomeController.php

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -5924,6 +5924,57 @@ class HomeController extends Controller
 			            'level' => '4',
 			            'color' => '#359154',
 			        ]],
+			        'nodes' => [[
+			            'id' => 'Shareholders'
+			        ], [
+			            'id' => 'Board'
+			        ], [
+			            'id' => 'CEO',
+			            'title'  => 'CEO',
+			            'name' => 'Grethe Hjetland',
+			            'image' => 'https://wp-assets.highcharts.com/www-highcharts-com/blog/wp-content/uploads/2020/03/17131126/Highsoft_03862_.jpg'
+			        ], [
+			            'id' => 'HR',
+			            'title'  => 'HR/CFO',
+			            'name' => 'Anne Jorunn Fjærestad',
+			            'color' => '#007ad0',
+			            'image' => 'https://wp-assets.highcharts.com/www-highcharts-com/blog/wp-content/uploads/2020/03/17131210/Highsoft_04045_.jpg'
+			        ], [
+			            'id' => 'CTO',
+			            'title'  => 'CTO',
+			            'name' => 'Christer Vasseng',
+			            'image' => 'https://wp-assets.highcharts.com/www-highcharts-com/blog/wp-content/uploads/2020/03/17131120/Highsoft_04074_.jpg'
+			        ], [
+			            'id' => 'CPO',
+			            'title'  => 'CPO',
+			            'name' => 'Torstein Hønsi',
+			            'image' => 'https://wp-assets.highcharts.com/www-highcharts-com/blog/wp-content/uploads/2020/03/17131213/Highsoft_03998_.jpg'
+			        ], [
+			            'id' => 'CSO',
+			            'title' => 'CSO',
+			            'name' => 'Anita Nesse',
+			            'image' => 'https://wp-assets.highcharts.com/www-highcharts-com/blog/wp-content/uploads/2020/03/17131156/Highsoft_03834_.jpg'
+			        ], [
+			            'id' => 'Product',
+			            'name' => 'Product developers'
+			        ], [
+			            'id' => 'Web',
+			            'name' => 'Web devs, sys admin'
+			        ], [
+			            'id' => 'Sales',
+			            'name' => 'Sales team'
+			        ], [
+			            'id' => 'Market',
+			            'name' => 'Marketing team',
+			            'column' => '5'
+			        ]],
+			        'colorByPoint' => 'false',
+			        'color' => '#007ad0',
+			        'dataLabels' => [
+			            'color' => 'white'
+			        ],
+			        'borderColor' => 'white',
+			        'nodeWidth' => '65'
 				]
 			)
 			->display(0);
