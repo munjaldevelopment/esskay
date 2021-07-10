e587b70b06c5c146b0094c0ae398ac1af908b105
Munjal Mayank <munjaldevelopment@gmail.com>
2021-07-10 18:23:13 +0530

    Update HomeController.php

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -5563,8 +5563,7 @@ class HomeController extends Controller
 					],
 					'title' => [
 						'text' => INCREMENTAL_LABEL1
-					],
-					'opposite' => true
+					]
 				],
 				[
 					//'gridLineWidth' => '0',
@@ -5576,7 +5575,8 @@ class HomeController extends Controller
 						'style' => [
 	                    	'fontWeight' => 'bold',
 	                    ]
-					]
+					],
+					'opposite' => true
 				],
 				[
 					//'gridLineWidth' => '0',
