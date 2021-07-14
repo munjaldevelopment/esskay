dfa0aee8be00c3c257f8ba3825c5397b4f796ea4
Munjal Mayank <munjaldevelopment@gmail.com>
2021-07-14 11:34:23 +0530

    Update sanction-letter-info.blade.php

--- resources/views/sanction-letter-info.blade.php
+++ resources/views/sanction-letter-info.blade.php
@@ -30,7 +30,7 @@
 								<td>{{ $row->roi }}</td>
 								<td>{{ $row->processing_fees }}</td>
 								<td>@if($row->status == 1) Accepted @elseif($row->status == 2) Rejected @else Pending @endif</td>
-								<td>{{ $row->sanction_date }}</td>
+								<td>{{ date('d/m/Y', strtotime($row->sanction_date)) }}</td>
 								<td class="">
 									<a class="btn btn-info display-sanction" href="javascript:;" data-id="{{ $row->id }}"><i class="fa fa-eye"></i></a>
 								</td>
