10724814ce0cca521ffcbb997e791ddbc0921899
Munjal Mayank <munjaldevelopment@gmail.com>
2021-07-17 13:23:50 +0530

    html

--- resources/views/insight-listing-trustee.blade.php
+++ resources/views/insight-listing-trustee.blade.php
@@ -1022,7 +1022,7 @@
 	}
 </style>
 <script type="text/javascript">
-    OrgChart.templates.isla.html = '<foreignobject class="node" x="20" y="10" width="200" height="100">{val}</foreignobject>';
+    OrgChart.templates.isla.html = '<foreignobject fill="#fff" class="node" x="20" y="10" width="200" height="100">{val}</foreignobject>';
     var chart = new OrgChart(document.getElementById("operational_chart"), {
         template: "isla",
         enableSearch: false,
@@ -1072,7 +1072,7 @@
 	}
 </style>
 <script type="text/javascript">
-	OrgChart.templates.isla.html = '<foreignobject class="node" x="20" y="10" width="200" height="100">{val}</foreignobject>';
+	OrgChart.templates.isla.html = '<foreignobject fill="#fff" class="node" x="20" y="10" width="200" height="100">{val}</foreignobject>';
 
 	var chart = new OrgChart(document.getElementById("hierarchy_chart"), {
         template: "isla",
